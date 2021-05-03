58829c1dc2b02f826c420bd0ba6f82b68ea56ead
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-03 11:25:54 -0500

    Validando parámetros con class-validator y mapped-types

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,17 +1,38 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
   // Creamos atributos de solo lectura y son requeridos
+  @IsString() // Decorador para validar String
+  @IsNotEmpty() // Decorador para validar campos no vacios
   readonly name: string;
+
+  @IsString() // Decorador para validar String
+  @IsNotEmpty() // Decorador para validar campos no vacios
   readonly description: string;
+
+  @IsNumber() // Decorador para validar numeros
+  @IsNotEmpty() // Decorador para validar campos no vacios
+  @IsPositive() // Decorador para validar numeros positivos
   readonly price: number;
+
+  @IsNumber() // Decorador para validar numeros
+  @IsNotEmpty() // Decorador para validar campos no vacios
+  @IsPositive() // Decorador para validar numeros positivos
   readonly stock: number;
+
+  @IsUrl() // Decorador para validar Url
+  @IsNotEmpty() // Decorador para validar campos no vacios
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  // Creamos atributos de solo lectura y pueden ser opcionales con  el signo ?
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+// PartialType permite reutilizar codigo utilizarndo las mismas validaciones y caracteristicas del clase que se extiende (CreateProductDto)
+// y simplemente los atributos los hace opcionales
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
