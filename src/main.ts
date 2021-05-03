9f9c24bc3ea188516dcb767d6eb843db39f80523
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-03 11:38:12 -0500

    Evitando parámetros incorrectos o no definidos

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Activamos el pipe para poder ser utilizado
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
